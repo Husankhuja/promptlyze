826011f1a9775530812a9968eabf10944826a9e8
Mike Odnis <95197809+WomB0ComB0@users.noreply.github.com>
2023-11-14 19:21:26 -0500

    Quick commit

--- api/index.py
+++ api/index.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
-@app.route("/api/python")
+
+@app.route("/api/python", methods=["GET"])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    data = {"message": "Hello, World!"}
+    return jsonify({"greeting": "Hello", "target": "World"})
+
+if __name__ == "__main__":
+    app.run(port=5328)
