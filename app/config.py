c7d0ef1b74bc3c4ee7336695c267fc45600cdd92
Husankhuja <56573502+Husankhuja@users.noreply.github.com>
2024-03-02 06:28:46 -0500

    feat: load confidential keys from env

--- app/config.py
+++ app/config.py
@@ -1,6 +1,11 @@
+import os
+from dotenv import load_dotenv 
+
+load_dotenv()
+
 config = {
     'SQLALCHEMY_DATABASE_URI' : 'sqlite:///mydb.db',
-    'JWT_SECRET_KEY' : 'your-secret-key',
+    'JWT_SECRET_KEY' : os.getenv('JWT_SECRET_KEY', 'test-key'),
     'SQLALCHEMY_TRACK_MODIFICATIONS' : False,
     'UPLOAD_FOLDER' : './documents/',
 }
